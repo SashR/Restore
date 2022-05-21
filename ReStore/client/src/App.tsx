2959c79fcf4460f76e09c92f78fe061662439649
SashR <sashinramdhani@gmail.com>
2022-05-21 11:13:24 +0200

    Basic concepts

--- ReStore/client/src/App.tsx
+++ ReStore/client/src/App.tsx
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+
+const products = [
+  {name: 'Product1', price: 100.00, key:"1"},
+  {name: 'Product2', price: 200.00, key:"2"},
+];
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Restore</h1>
+      <ul>
+        {products.map(prod => (
+          <li key={prod.key}>
+            <div> <b>Name:</b> {prod.name} </div> 
+            <div> <b>Price:</b> {prod.price} </div> 
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
