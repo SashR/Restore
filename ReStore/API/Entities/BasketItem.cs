4d3693624782a3cfba71571647f24cf08e34e2af
SashR <sashinramdhani@gmail.com>
2022-06-18 11:07:18 +0200

    Properly setup one to many relationship
    Basket and BasketItem

--- ReStore/API/Entities/BasketItem.cs
+++ ReStore/API/Entities/BasketItem.cs
@@ -1,13 +1,22 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+    [Table("BasketItems")]
     public class BasketItem
     {
         public int Id {get; set;}
 
         public int Quantity {get; set;}
 
+        // Navigation properties
         public int ProductId {get; set;}
 
         public Product Product {get; set;}
+
+        // Fully definitions
+        public int BasketId {get; set;}
+
+        public Basket Basket {get; set; }
     }
 }
