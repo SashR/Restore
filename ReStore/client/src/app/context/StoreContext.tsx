9445b0f28e258fd1b2ccc539e0e6be1d6a548036
SashR <sashinramdhani@gmail.com>
2022-06-21 20:13:19 +0200

    Centralizing state

---
+++ ReStore/client/src/app/context/StoreContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+import { Basket } from "../models/basket";
+
+interface StoreContextValue {
+    basket: Basket | null;
+    setBasket: (basket: Basket) => void;
+    removeItem: (productId: number, quantity: number) => void;
+    // we get the basket back when adding an item, so we can use setItem for that
+}
+
+export const StoreContext = createContext<StoreContextValue | undefined>(undefined);
+
+// Custom react hook to use context easily
+export const useStoreContext = () => {
+    const context = useContext(StoreContext);
+    if (context === undefined){
+        throw Error('Not inside provider');
+    }
+
+    return context;
+}
+
+export const StoreProvider = ({children} : PropsWithChildren<any> ) => {
+    const [basket, setBasket] = useState<Basket | null>(null);
+
+    const removeItem = (productId: number, quantity: number) => {
+        if(!basket) return; // no basket
+        const items = [...basket.items]; // copy of items
+        const itemIndex = items.findIndex(i => i.productId === productId);
+        if(itemIndex >=0){      // ensure item is found
+            items[itemIndex].quantity -= 1;
+            // Remove item if quantity is zero
+            if(items[itemIndex].quantity === 0) items.splice(itemIndex, 1);
+            setBasket((prevState: any) => { return {...prevState, items}});
+        }
+    }
+
+    return (
+        <StoreContext.Provider value={{basket, setBasket, removeItem}}>
+            {children}
+        </StoreContext.Provider>
+    )
+}
